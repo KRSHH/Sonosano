3977d4339f66ecf7c76a87cd6807071d5b7f1da1
KRSHH <136873090+KRSHH@users.noreply.github.com>
2025-10-17 01:00:44 +0530

    Trim Pynicotine

--- backend/src/pynicotine/headless/__init__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2022-2023 Nicotine+ Contributors
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-
-def run():
-    """Run application in headless (no GUI) mode."""
-
-    from pynicotine.headless.application import Application
-    return Application().run()
